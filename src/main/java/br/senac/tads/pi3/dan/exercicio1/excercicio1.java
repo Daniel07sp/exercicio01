624613233dd5517e8374c82cf4ef1e6a0aa4066f
daniel.rodrigues1 <daniel.rodrigues1@CAS486190C14727.senacsp.edu.br>
2015-08-14 20:06:25 -0300

    Inclusao do metodo main

---
+++ src/main/java/br/senac/tads/pi3/dan/exercicio1/excercicio1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.dan.exercicio1;
+
+/**
+ *
+ * @author daniel.rodrigues1
+ */
+public class excercicio1 {
+    public static void main(String[] args) {
+        System.out.println("ola");
+    }
+    
+}
